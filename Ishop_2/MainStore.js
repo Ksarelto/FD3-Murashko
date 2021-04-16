3219f1ad46b1b9f87ef140608c2551281045a92a
Artsiom <77408026+Ksarelto@users.noreply.github.com>
2021-04-17 02:25:11 +0300

    fix: add new state "newItemsList", change func deleteAllRow

---
+++ Ishop_2/MainStore.js
@@ -0,0 +1,70 @@
+const MainStore = React.createClass({
+
+	displayName: 'Ishop',
+
+	getDefaultProps: function(){
+		return {shop: 'Ishop'}
+	},
+
+	getInitialState: function(){
+		return{
+			newItemsList: this.props.items,
+			checkedRow: null,
+			deletedRows: {},
+		};
+	},
+
+	rowSelected: function(code) {
+	    console.log('выбран ответ с кодом '+code);
+	    this.setState( {checkedRow:code} );
+	    console.log(this.state.newItemsList[0].code);
+	 },
+
+	 deleteAllRow: function(bul,code){
+	 	if(bul){
+	 		let result = [];
+	 		this.state.newItemsList.forEach( (el) => {
+	 			if(el.code !== code){
+	 				result.push(el); 
+	 			}
+	 		} );
+
+	 		this.state.newItemsList = result;
+	 	}
+		
+	 },
+
+
+
+	render: function(){
+
+		const result = this.state.newItemsList.map( (el,ind) => {
+				if(el.code in this.state.deletedRows){
+					return;
+				}
+				if(el.code === this.state.checkedRow){
+					return React.createElement(IshopItemsContainer, {key:el.code,
+							itemName: el.itemName,code:el.code, price: el.price, rest: el.rest,
+							url: el.url, backgroundStyle: "red", msRowSelected: this.rowSelected, deleteAllRow: this.deleteAllRow});
+				}
+				return React.createElement(IshopItemsContainer, {key:el.code,
+					itemName: el.itemName, price: el.price,code:el.code, rest: el.rest,
+					url: el.url, backgroundStyle: "", msRowSelected: this.rowSelected, deleteAllRow: this.deleteAllRow});
+		})
+
+		return React.DOM.div({className: 'ishop'},
+				React.DOM.h1({className: 'ishop__header'}, this.props.shop),
+				React.DOM.div({className: 'ishop__container'}, 
+					React.DOM.div({className:"ishop__item table-header"},
+						React.DOM.span({className: "table-header__items table-header__items_img"}, "Foto"),
+						React.DOM.span({className: "table-header__items table-header__items_name"}, "Name"),
+						React.DOM.span({className: "table-header__items table-header__items_price"}, "Price"),
+						React.DOM.span({className: "table-header__items table-header__items_rest"}, "Left" ),
+						React.DOM.span({className: "table-header__items table-header__items_button"}, "Control" )
+					),
+					React.DOM.div({className: "ishop__table"}, result)
+				)
+			)
+
+	},
+})
