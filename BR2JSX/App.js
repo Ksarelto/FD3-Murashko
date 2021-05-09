db4a128b0efa7b15b02246b89e20bf01976b838a
Artsiom <77408026+Ksarelto@users.noreply.github.com>
2021-05-09 17:45:23 +0300

    Add files via upload

---
+++ BR2JSX/App.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MultilineText from './components/MultilineText';
+
+const text = require('./multilineText.json');
+
+
+ReactDOM.render(
+  <MultilineText
+    multilineText={text}
+  />
+  , document.getElementById('container')
+);
+
