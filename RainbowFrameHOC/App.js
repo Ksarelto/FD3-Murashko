5f74b5a3fe9a3e2bfabcde2cbca6b38511f8682d
Artsiom <77408026+Ksarelto@users.noreply.github.com>
2021-05-09 17:35:36 +0300

    Add files via upload

---
+++ RainbowFrameHOC/App.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './components/RainbowFrame.css'
+import MainComponent from './components/MainComponent'
+let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple']
+ReactDOM.render(
+  <MainComponent colors={colors}></MainComponent>
+  , document.getElementById('container')
+);
+
