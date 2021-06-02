0431a3cdf3e1acc3b07958c4e656c0a6f48e5c9d
Artsiom Murashko <ksarelto@tut.by>
2021-06-02 20:10:32 +0300

    refactor: add types to methods and propertyies

--- Scales/src/modules/Scales.ts
+++ Scales/src/modules/Scales.ts
@@ -1,15 +1,17 @@
-export class Scales{
-  productsArr = [];
+import { Product } from "./Product";
 
-  addProduct(item: Object){
+export class Scales{
+  productsArr: Array<Product> = [];
+
+  addProduct(item: Product){
       this.productsArr.push(item);
   }
 
-  get sumScale(){
+  get sumScale():number{
       return this.productsArr.reduce((sum,el) => sum += el.weight ,0)
   }
 
-  get nameList(){
+  get nameList():Array<string>{
       return this.productsArr.map((el) => el.name)
   }
 }
